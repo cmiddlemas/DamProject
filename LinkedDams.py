4c2ea02010016299eafaa733ebe98e4fa19eaa14
tmiddlemas <tmiddlemas@hmc.edu>
2017-01-20 16:15:14 -0800

    More Script

--- LinkedDams.py
+++ LinkedDams.py
@@ -6,80 +6,139 @@ Handles tributaries and spillways, integrates by forward stepping.
 Author: Tim Middlemas
 """
 
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib as mpl
 import itertools
 
 class dam():
-    def __init__(self, V, R, Vmax):
+    def __init__(self, V, Vmax, CV, CI, overflow, R):
         self.V = V #current volume of water in dam
-        self.R = R #rate of outflow in dam
         self.Vmax = Vmax #maximum carrying capacity of dam
+        self.CV = CV
+        self.CI = CI
+        self.overflow = overflow
+        self.R = R
         
-def step(damTree,dt):
+def control(dam, vol, dt):
+    #Determine outflow, assuming no over or underflow
+    flowVol = dam.V*dam.CV*dt + dam.CI*vol + dam.R*dt
+    #print flowVol
+    dam.V += vol
+    dam.V -= flowVol
+    #handle underflow
+    if dam.V < 0.0:
+        out = flowVol + dam.V        
+        dam.V = 0.0
+        return out
+    else:
+        #handle overflow
+        if dam.V > dam.Vmax:
+            out = dam.V - dam.Vmax
+            dam.V = dam.Vmax
+            dam.overflow += out
+            #print out
+            return out + flowVol
+        else:
+            return flowVol
+        
+    
+
+def step(damTree, dt):
     #Base Case, resolve flow:
     if len(damTree) == 1:
-        flowVol = dt*damTree[0].R
-        underflow = flowVol - damTree[0].V
-        #Make sure there's enough water in the dam to satisfly flow        
-        if underflow > 0.0:
-            outflow = damTree[0].V
-            damTree[0].V = 0.0
-            return outflow
-        else:
-            damTree[0].V -= flowVol
-            return flowVol
+        return control(damTree[0], 0.0, dt)
     #Else Case, recursively resolve flow
     else:
         #Add the flows from the child nodes
         vol = sum(map(lambda x: step(x, dt), damTree[1:]))
-        #Does the dam overflow?
-        if vol + damTree[0].V > damTree[0].Vmax :
-            return vol
-        else:
-            damTree[0].V += vol
-            flowVol = dt*damTree[0].R
-            underflow = flowVol - damTree[0].V
-            #Make sure there's enough water in the dam to satisfly flow        
-            if underflow > 0.0:
-                outflow = damTree[0].V
-                damTree[0].V = 0.0
-                return outflow
-            else:
-                damTree[0].V -= flowVol
-                return flowVol
+        return control(damTree[0],vol, dt)
                 
 def extract_vol(damList):
     return map(lambda x: x.V, damList)
+    
+def extract_overflow(damList):
+    return map(lambda x: x.overflow, damList)
         
 def make_plots(times,vols):
     mpl.pyplot.plot(times,vols)
     nDams = len(vols[0])
     mpl.pyplot.legend(map(lambda x: str(x), range(nDams)))
-    mpl.pyplot.axis([0.0,30.0,0.0,0.7])
+    #mpl.pyplot.axis([0.0,30.0,0.0,12.0])
     
 def run_simulation(damTree,dt,nSteps,damList):
     t = 0.0
     vols = []
     times = []
+    outflows = []
+    overflows = []
     for i in range(nSteps):
         vols += [extract_vol(damList)]
         times += [t]
-        step(damTree,dt)
+        outflows += [step(damTree,dt)/dt]
+        overflows += [extract_overflow(damList)]
         t += dt
     vols += [extract_vol(damList)]
     times += [t]
+    overflows += [extract_overflow(damList)]
+    mpl.pyplot.figure(0)
     make_plots(times,vols)
+    mpl.pyplot.figure(1)
+    mpl.pyplot.scatter(times[:-1],outflows)
+    mpl.pyplot.figure(2)
+    make_plots(times,overflows)
     return
 
+def evaluate_overflow(damTree,dt,nSteps,damList):
+    for i in range(nSteps):
+        step(damTree, dt)
+    return damTree[0].overflow
+    
 #Specify data here
+    
+tVol = 100000000.0
+tCap = 1000000000000.0
+C1 = 0.01
+C2 = 0.9
 
 #Define dams with initial parameters
-a1 = dam(1.0,0.1,1.0)
-a2 = dam(1.0,0.1,1.0)
-b = dam(0.0,0.1,5.0)
-c = dam(0.0,0.0,0.5)
+# We take flow rates by using mean yearly rate in m^3/s from map one
+# Rates are now in km^3/yr
+kariba = dam(20.0, 25.0, C1, C2, 0.0, 0.0) 
+tKariba = dam(tVol,tCap, 0.0,0.0,0.0, 40.0) 
+victoria = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tVictoria = dam(tVol,tCap,0.0,0.0,0.0, 4.4) 
+d8 = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tD8 = dam(tVol,tCap,0.0,0.0,0.0, 1.04) 
+d9 = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tD9 = dam(tVol,tCap,0.0,0.0,0.0,38.0) 
+d10 = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tD10 = dam(tVol,tCap,0.0,0.0,0.0,2.2) 
+d11 = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tD11 = dam(tVol,tCap,0.0,0.0,0.0,3.6) 
+d12 = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tD12 = dam(tVol,tCap,0.0,0.0,0.0,24.0) 
+d13 = dam(20.0, 25.0, C1, C2, 0.0, 0.0)
+tD13 = dam(tVol,tCap,0.0,0.0,0.0,8.7) 
+
 
 #Define dam topology and provide dam list
-dTree = [c,[b,[a1],[a2]]]
-dList = [c,b,a1,a2]
+dTree = [kariba,[victoria,[d8,[tD8]],[d9,[d10,[tD10]],[d11,[tD11]],[d12,[tD12]],[d13,[tD13]],[tD9]],[tVictoria]],[tKariba]]
+dList = [kariba,victoria,d8,d9,d10,d11,d12,d13]
+
+def plot_energy():
+    oArray = np.zeros((10,10))
+    for i in range(10):
+        print i
+        for j in range(10):
+            inflow = dam(10000000.0,1000000000000.0,0.0,0.0,0.0,1.0)
+            a = dam(0.0,10.0,0.01*i,0.05*j,0.0,0.0)
+            b = dam(0.0,10.0,0.01*i,0.05*j,0.0,0.0)
+            testTree = [b,[a,[inflow]]]
+            testList = [a,b]
+            oArray[i][j] = evaluate_overflow(testTree,0.1,10000,testList)
+    mpl.pyplot.imshow(oArray,cmap = mpl.cm.coolwarm,interpolation = 'nearest')
+    print oArray[0][0]
+    print oArray[1][0]
+    print oArray[0][1]
+    
