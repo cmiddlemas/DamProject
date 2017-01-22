e81b8158ab9acbaff73dc46a8eb260a39f4e508f
Calvin Leung <cleung@g.hmc.edu>
2017-01-21 20:20:09 -0800

    fixed this

--- rainfallData.py
+++ rainfallData.py
@@ -1,12 +1,33 @@
 import pandas as pd
-
-
+import numpy as np
+from scipy import interpolate
 rainfall = pd.read_excel(open("basin_flow_data.xlsx","rb"), sheetname='Sheet1')
 table = rainfall.values
-byRegion = table[3:15,:].transpose()
+byRegion = table.transpose()[1:,0:12]
 
-[norm8,drought8,norm9,drought9,norm10,drought10,norm11,drought11,norm12,drought12,norm13,drought13,norm6,drought6,normvictoria,droughtvictoria,normkariba,droughtkariba]   = byRegion[1:]
-months = np.arange(12) + 1
+
+[norm8,drought8,norm9,drought9,norm10,drought10,norm11,drought11,norm12,drought12,norm13,drought13,norm6,drought6,normvictoria,droughtvictoria,normkariba,droughtkariba] = byRegion
+months = np.arange(12)
+
+
+norm8 = interpolate.interp1d(months,norm8,kind = 'cubic')
+norm9 = interpolate.interp1d(months,norm9,kind = 'cubic')
+norm10 = interpolate.interp1d(months,norm10,kind = 'cubic')
+norm11 = interpolate.interp1d(months,norm11,kind = 'cubic')
+norm12 = interpolate.interp1d(months,norm12,kind = 'cubic')
+norm13 = interpolate.interp1d(months,norm13,kind = 'cubic')
+normvictoria = interpolate.interp1d(months,normvictoria,kind = 'cubic')
+normvictoria = interpolate.interp1d(months,normkariba,kind = 'cubic')
+
+drought8 = interpolate.interp1d(months,drought8,kind = 'cubic')
+drought9 = interpolate.interp1d(months,drought9,kind = 'cubic')
+drought10 = interpolate.interp1d(months,drought10,kind = 'cubic')
+drought11 = interpolate.interp1d(months,drought11,kind = 'cubic')
+drought12 = interpolate.interp1d(months,drought12,kind = 'cubic')
+drought13 = interpolate.interp1d(months,drought13,kind = 'cubic')
+droughtvictoria = interpolate.interp1d(months,droughtvictoria,kind = 'cubic')
+droughtvictoria = interpolate.interp1d(months,droughtkariba,kind = 'cubic')
+"""
 Print = True
 if Print:
     plt.figure()
@@ -33,3 +54,4 @@ if Print:
     plt.plot(months,drought13,label='13')
     plt.plot(months,droughtvictoria,label='Victoria')
     plt.plot(months,droughtkariba,label='Kariba')
+"""
